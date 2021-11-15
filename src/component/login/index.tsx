3b18d101cb947ab30a841447d094b24f29a2176f
Carlos-Handsome-TJ <201821100479@smail.xtu.edu.cn>
2021-11-15 21:32:33 +0800

    fix:测试

--- src/component/login/index.tsx
+++ src/component/login/index.tsx
@@ -17,52 +17,53 @@ const Login = () => {
     }
   };
   return (
-    <Body>
-      <LoginContainer>
-        <Title>欢迎登录小可爱的网站</Title>
-        <Form
-          name="normal_login"
-          className="login-form"
-          initialValues={{ remember: true }}
-          onFinish={onFinish}
-        >
-          <Form.Item
-            name="username"
-            rules={[{ required: true, message: "Please input your Username!" }]}
-          >
-            <Input
-              prefix={<UserOutlined className="site-form-item-icon" />}
-              placeholder="Username"
-            />
-          </Form.Item>
-          <Form.Item
-            name="password"
-            rules={[{ required: true, message: "Please input your Password!" }]}
-          >
-            <Input
-              prefix={<LockOutlined className="site-form-item-icon" />}
-              type="password"
-              placeholder="Password"
-            />
-          </Form.Item>
-          <Form.Item>
-            <Form.Item name="remember" valuePropName="checked" noStyle>
-              <Checkbox>Remember me</Checkbox>
-            </Form.Item>
-          </Form.Item>
-          <Form.Item>
-            <Button
-              type="primary"
-              htmlType="submit"
-              className="login-form-button"
-              size="middle"
-            >
-              登录
-            </Button>
-          </Form.Item>
-        </Form>
-      </LoginContainer>
-    </Body>
+    <>2222测试代码结构表示</>
+    // <Body>
+    //   <LoginContainer>
+    //     <Title>欢迎登录小可爱的网站2222</Title>
+    //     <Form
+    //       name="normal_login"
+    //       className="login-form"
+    //       initialValues={{ remember: true }}
+    //       onFinish={onFinish}
+    //     >
+    //       <Form.Item
+    //         name="username"
+    //         rules={[{ required: true, message: "Please input your Username!" }]}
+    //       >
+    //         <Input
+    //           prefix={<UserOutlined className="site-form-item-icon" />}
+    //           placeholder="Username"
+    //         />
+    //       </Form.Item>
+    //       <Form.Item
+    //         name="password"
+    //         rules={[{ required: true, message: "Please input your Password!" }]}
+    //       >
+    //         <Input
+    //           prefix={<LockOutlined className="site-form-item-icon" />}
+    //           type="password"
+    //           placeholder="Password"
+    //         />
+    //       </Form.Item>
+    //       <Form.Item>
+    //         <Form.Item name="remember" valuePropName="checked" noStyle>
+    //           <Checkbox>Remember me</Checkbox>
+    //         </Form.Item>
+    //       </Form.Item>
+    //       <Form.Item>
+    //         <Button
+    //           type="primary"
+    //           htmlType="submit"
+    //           className="login-form-button"
+    //           size="middle"
+    //         >
+    //           登录
+    //         </Button>
+    //       </Form.Item>
+    //     </Form>
+    //   </LoginContainer>
+    // </Body>
   );
 };
 
