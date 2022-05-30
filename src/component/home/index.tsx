58606a8e81fa2dee8571b5b956e0203d2d01a6f8
Carlos-Handsome-TJ <201821100479@smail.xtu.edu.cn>
2022-05-30 21:12:15 +0800

    增加导航组件

--- src/component/home/index.tsx
+++ src/component/home/index.tsx
@@ -1,7 +1,25 @@
 import * as React from "react";
+import Nav from "./nav";
+import styled from "styled-components";
 
 const HomePage = React.memo(() => {
-  return <>啦啦啦啦，希望小可爱开开心心的，忘掉烦心事</>;
+  return (
+    <>
+      <HomeContainer>2222</HomeContainer>
+    </>
+  );
 });
 
 export default HomePage;
+
+const HomeContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  background: orange;
+  @media (max-width: 600px) {
+  }
+  @media (min-width: 600px) and (max-width: 1200px) {
+  }
+  @media (min-width: 1200px) {
+  }
+`;
---
+++ src/component/home/nav.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+
+const Nav = React.memo(() => {
+  return <div>导航栏组件</div>;
+});
+
+export default Nav;
