62ae6fe9e5fd67499c11a7b4eacb6ee25ea5263b
Carlos-Handsome-TJ <201821100479@smail.xtu.edu.cn>
2022-06-05 12:36:41 +0800

    fix:导航组件样式调整

--- src/component/home/nav.tsx
+++ src/component/home/nav.tsx
@@ -1,6 +1,11 @@
 import * as React from "react";
 import styled from "styled-components";
 
+interface initialNavList {
+  name: string;
+  icon: string;
+}
+
 const Nav = React.memo(() => {
   return (
     <NavContainer>
@@ -9,26 +14,35 @@ const Nav = React.memo(() => {
   );
 });
 
-const ToolList = React.memo(() => {
+const ToolList: React.FC<{}> = React.memo(() => {
+  const [navList, setNavList] = React.useState<initialNavList[] | null>([
+    { name: "首页", icon: "#icon-shutiao" },
+    { name: "动态", icon: "#icon-zhenzhunaicha" },
+    { name: "热门", icon: "#icon-xuegao" },
+    { name: "频道", icon: "#icon-kele" },
+  ]);
   return (
     <>
       <UlContainer>
-        <LiContainer>
-          <i className={"iconfont"}>&#xe658;</i>
-          首页
-        </LiContainer>
-        <LiContainer>
-          <i className={"iconfont"}>&#xe659;</i>
-          动态
-        </LiContainer>
-        <LiContainer>
-          <i className={"iconfont"}>&#xe65a;</i>
-          热门
-        </LiContainer>
-        <LiContainer>
-          <i className={"iconfont"}>&#xe65c;</i>
-          频道
-        </LiContainer>
+        {navList &&
+          navList.map((item, index) => {
+            return (
+              <LiContainer key={item.name}>
+                <AContainer>
+                  <IconDivContainer>
+                    <svg
+                      className="icon"
+                      style={{ fontSize: "2rem" }}
+                      aria-hidden="true"
+                    >
+                      <use xlinkHref={item.icon}></use>
+                    </svg>
+                    <IConPContainer>{item.name}</IConPContainer>
+                  </IconDivContainer>
+                </AContainer>
+              </LiContainer>
+            );
+          })}
       </UlContainer>
     </>
   );
@@ -37,21 +51,35 @@ const ToolList = React.memo(() => {
 const NavContainer = styled.div`
   width: 100%;
   height: 100vh;
-  //background: blue;
 `;
 const UlContainer = styled.ul`
   display: flex;
   width: 100%;
   height: 5rem;
-  line-height: 5rem;
-  //background: red;
   list-style: none;
   margin: 0;
-  padding: 0;
+  padding: 5rem 3rem;
   justify-content: space-between;
+  align-items: center;
 `;
 const LiContainer = styled.li`
-  padding: 0 1rem;
+  width: 3.5rem;
+  height: 3.5rem;
+  background: skyblue;
+  box-sizing: border-box;
+  border-radius: 50%;
 `;
+const AContainer = styled.a`
+  display: inline-block;
+  width: 3.5rem;
+  height: 3.5rem;
+  color: #000;
+`;
+const IconDivContainer = styled.div`
+  width: 3.5rem;
+  height: 3.5rem;
+  text-align: center;
+`;
+const IConPContainer = styled.p``;
 
 export default Nav;
